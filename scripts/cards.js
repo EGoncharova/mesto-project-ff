bce1e75377355a6db63e29fcb84b0a996367d135
Ekaterina Goncharova <nini3@yandex.ru>
2023-12-23 12:18:18 +0300

    First Try

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
       name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      link: "./images/card_1.jpg",
     },
     {
       name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      link: "./images/card_2.jpg",
     },
     {
       name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      link: "./images/card_3.jpg",
     },
     {
       name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      link: "./images/card_4.jpg",
     },
     {
       name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      link: "./images/card_5.jpg",
     },
     {
       name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      link: "./images/card_6.jpg",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 // @todo: Темплейт карточки
 
+
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
@@ -7,3 +8,32 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+
+const del = function(evt) {
+    evt.target.closest('.card').remove();
+}
+
+function addCard(link, name, callback) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    card.querySelector('.card__image').src= link;
+    card.querySelector('.card__title').innerText = name;
+    card.querySelector('.card__delete-button').addEventListener('click', callback);
+    return card;
+}
+
+// цикл фор позволяет пере,рать поштучно элементы массива
+// но его можэно заменить на метод массива forEach
+        
+// for (let index = 0; index < initialCards.length; index++) {
+
+//     placesList.append(addCard( initialCards[index].link, initialCards[index].name, del));
+   
+// }
+
+//  forEach принимает в качестве параметра фцию-колбэк, параметр которой определяет, как мы будем обращаться к каждому отдельному элементу массива
+initialCards.forEach(function(item) {
+    placesList.append(addCard(item.link, item.name, del));
+});
